6b16574aac78344895e8ee15f80cb4785765c2a8
Harsha Vardhan Kandimalla <scoutlineco@gmail.com>
2026-04-17 17:31:02 +0530

    fix: exhaustive open-next config properties

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,19 +1,24 @@
-const config = {
+export default {
     default: {
         override: {
             wrapper: "cloudflare-node",
             converter: "edge",
             proxyExternalRequest: "fetch",
+            incrementalCache: "dummy",
+            tagCache: "dummy",
+            queue: "direct",
         },
     },
+    edgeExternals: ["node:crypto"],
     middleware: {
         external: true,
         override: {
             wrapper: "cloudflare-edge",
             converter: "edge",
             proxyExternalRequest: "fetch",
+            incrementalCache: "dummy",
+            tagCache: "dummy",
+            queue: "direct",
         },
     },
 };
-
-export default config;
